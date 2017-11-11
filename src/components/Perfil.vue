4676afea18f219feea36a0a35bc33bfffc100ba9
jjvg <jjvg_12@hotmail.com>
2017-11-10 23:57:12 -0400

    Merge branch 'master' of https://github.com/jjvg/red_movil
    # Conflicts:
    #	www/index.html
    #	www/static/css/app.999ebf126d951598437c874acbf0d8eb.css
    #	www/static/css/app.cdc77171da362b1501e674f1bb9ad873.css
    #	www/static/css/app.eedcff9b6abf22109b1f61d9eeea63b4.css
    #	www/static/js/app.95d9ae377f761b8f26c0.js
    #	www/static/js/manifest.3ec74e90536fb0e2a46b.js
    #	www/static/js/manifest.3ec74e90536fb0e2a46b.js.map
    #	www/static/js/manifest.4b96b89014cb500dff39.js
    #	www/static/js/manifest.4b96b89014cb500dff39.js.map
    #	www/static/js/manifest.acacc25391bad4174198.js
    #	www/static/js/manifest.acacc25391bad4174198.js.map

---
+++ src/components/Perfil.vue
@@ -0,0 +1,61 @@
+<template>
+    <v-ons-page id="app">
+       <!--<v-ons-toolbar>
+      <div class="center">Perfil</div>
+    </v-ons-toolbar>-->
+
+    
+    <v-ons-card>
+       <img src="../assets/perfil.jpg" alt="Onsen UI" style="border-radius: 50%; width: 150px;
+     height: 150px">
+          <div class="title">
+            Awesome framework
+          </div>
+      <div class="content">
+        <div>
+            <v-ons-button class="button--material"><v-ons-icon></v-ons-icon>Editar perfil</v-ons-button>
+        </div>
+        <v-ons-list>
+          <!--<v-ons-list-header>Bindings</v-ons-list-header>-->
+          <v-ons-list-item>Vue</v-ons-list-item>
+          <v-ons-list-item>Angular</v-ons-list-item>
+          <v-ons-list-item>React</v-ons-list-item>
+        </v-ons-list>
+      </div>
+    </v-ons-card>
+    
+      
+
+    </v-ons-page>
+        </template>
+<script>
+export default {
+  name: 'perfil'
+}
+</script>
+
+<style scoped>
+.img{
+     border-radius: 50%;
+     width: 150px;
+     height: 150px;
+     }
+
+.toolbar--material{
+    background-color: purple;
+}
+ons-card {
+  text-align: center;
+  
+}
+.button--material {
+  font-weight: 100;
+  font-size: 10px;
+  font-weight: bold;
+  font-family: 'Roboto' ;
+ 
+
+}
+
+
+</style>
