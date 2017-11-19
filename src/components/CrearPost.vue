40665ad8f41c3ab7f0a2e66c41a17092ff235130
jjvg <jjvg_12@hotmail.com>
2017-11-19 17:18:23 -0400

    listo

--- src/components/CrearPost.vue
+++ src/components/CrearPost.vue
@@ -16,7 +16,7 @@
             </div>
              <div class="col s12 m6 l4">
                 <div class="input-field">
-                    <v-file-input v-model="img" placeholder="Selecione una imagen"></v-file-input>
+                    <v-file-input v-model="imagen" placeholder="Selecione una imagen" id="img"></v-file-input>
                  </div>
             </div>
         </div>
@@ -28,15 +28,19 @@
                 </div>
            </div>
         </div>
-         <div class="row">
-             <div class="col s12 m6 l4">
-                     <div class="input-field">
-                    <v-select name="select" id="select" v-model="selectedItem" :item="items"></v-select>
-                    <label for="select">Seleccione el Area dirigida</label>
-                    </div>
-             </div>
-         </div>
-        
+         <v-ons-list>
+             <v-ons-list-item>
+                <div class="center">
+
+                 <v-ons-select material class="material" style="width: 80%" v-model="selectedItem" >
+                 <option v-for="item in items" :value="item.value" :key="item.key">
+              {{ item.text }}
+            </option>
+          </v-ons-select>
+
+        </div>
+      </v-ons-list-item>
+         </v-ons-list>
         <section style="margin: 20px">
              <router-link to="/home"><v-ons-button style="margin: 10px 0;" class="material" @click="savePost()">Publicar</v-ons-button></router-link>
              <router-link to="/home"><v-ons-button style="margin: 10px 0;" class="material" @click="regresar()">Cancelar</v-ons-button></router-link>  
@@ -54,26 +58,26 @@
 export default {
    name: 'crearpost',
 
-   data () {
-       return {
-           post:[
-            titulo, 
-            imagen,
-            contenido,
-            categoria
-           ],
-            items:[
-                'Seguridad',
-                'Salud',
-                'Deporte',
-                'Servicios' ,
-                'Arte',                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
-                'Trasporte',
-            ],
-            selectedItem:'ninguno',
+   data : function() {
+        return {
+           
+            titulo:{type:String},
+            titulo:'', 
+            imagen:{type:File},
+            contenido:{type:String},
+            categoria:{type:Boolean},
+            items: [
+        { text: 'Salud', value: 'Salud' },
+        { text: 'Seguridad', value: 'Seguridad' },
+        { text: 'Arte', value: 'Arte' }
+      ],
+      selectedItem: ''
         
 
        }
+       
+   
+      
    },
    methods: {
 
