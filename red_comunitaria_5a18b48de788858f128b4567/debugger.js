470f72d3ca964ea81f525ce63f6ec3c3b80fe45d
jjvg <jjvg_12@hotmail.com>
2017-11-24 21:28:17 -0400

    ok

--- red_comunitaria_5a18b48de788858f128b4567/debugger.js
+++ red_comunitaria_5a18b48de788858f128b4567/debugger.js
@@ -15,11 +15,11 @@ chrome.app.runtime.onLaunched.addListener(function() {
 
 	port = chrome.runtime.connect(hostId, {name: chrome.runtime.id});
 	port.onMessage.addListener(portHandler);
-	port.postMessage({type: 'onlaunch', projectId: '5a179427e78885303a8b456c'});
+	port.postMessage({type: 'onlaunch', projectId: '5a18b48de788858f128b4567'});
 
 	setInterval(function() {
 		try {
-			port.postMessage({type: 'ping', projectId: '5a179427e78885303a8b456c'});
+			port.postMessage({type: 'ping', projectId: '5a18b48de788858f128b4567'});
 		} catch(e) {
 			port = chrome.runtime.connect(hostId, {name: chrome.runtime.id});
 			port.onMessage.addListener(portHandler);
