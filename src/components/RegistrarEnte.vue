7a8d0bae452c72968eef3272408dc3df2aa4c92d
joselyn <joselynserranof@gmail.com>
2017-11-26 16:52:54 -0400

    reg ente y comunidad

--- src/components/RegistrarEnte.vue
+++ src/components/RegistrarEnte.vue
@@ -1,11 +1,16 @@
 <template>
     <v-ons-page id="app">
-		
+		<v-ons-toolbar class="toolbar--material">
+			<div class="left">
+				<router-link to="/home"><v-ons-back-button>Page 1</v-ons-back-button></router-link>
+			</div>
+			<div class="center"><img src='../assets/img/rc1.png'  class="logo"></div>
+        </v-ons-toolbar>
 <div class="container">
 	<div class="row">
 		<div class="col s12">
 			<div align="center">
-				<h2>Registro de Entes</h2>
+				<h3>Registro de Entes</h3>
 			</div>
 		</div>
 	</div>
@@ -41,30 +46,53 @@
 				<div class="ub"><h5>Ubicación</h5></div>
 				</div>
 				<br>
-				<div class="col s10 m8">
-					<v-select class="validate" :options="['Lara','Yaracuy', 'Zulia', 'Mérida', 'Dto Capital', 'Falcón']" :placeholder="'Estado'"></v-select>
-					<v-select class="validate" :options="['Barqto','San Felipe', 'Maracaibo', 'Mérida', 'Caracas', 'Coro']" :placeholder="'Ciudad'"></v-select>
-					<div class="input-field col s12 m6">
-         	 		<input id="dir" type="text" class="validate">
-          			<label for="dir">Dirección</label>
-        		</div>
-  				</div>
-			</div>
-			<div class="row1">
-				<div class="ic"><i class="material-icons prefix">build</i></div>
-				<div class="sel">
-				<v-select class="validate" :options="['Agua','Electricidad', 'Educación', 'Alimentación', 'Vialidad', 'Mascotas']" :placeholder="'Área a la que se dedica'"></v-select>
 				</div>
-			</div>
+			<label>Estado</label>
+                <div class="center">
+                    
+                
+                    <v-ons-select name="edo" material class="material" style="width: 80%" v-model="selectedItem" >
+                        <option class="tam" v-for="item1 in edos" :value="item1.value" :key="item1.key">
+                            {{ item1.text }}
+                        </option>
+                    </v-ons-select>
+                
+                </div>
+                <label>Ciudad</label>
+                <div class="center">
+                    
+                
+                    <v-ons-select name="ciudad" material class="material" style="width: 80%" v-model="selectedItem" >
+                        <option class="tam" v-for="item2 in ciudad" :value="item2.value" :key="item2.key">
+                            {{ item2.text }}
+                        </option>
+                    </v-ons-select>
+                
+                </div>
+                <div class="col s12 m12 l6">
+               		<div class="input-field">
+						   
+						<v-text-area name="contenido" id="contenido" length="50" v-model="contenido"></v-text-area>
+						<label for="text"><i class="material-icons">pin_drop</i>Dirección</label>
+               		 </div>
+           		</div>
+				   <label>Área a la que se dedica</label>
+			<div class="center">
+                    
+                
+                    <v-ons-select name="area" material class="material" style="width: 80%" v-model="selectedItem" >
+                        <option class="tam" v-for="item in items" :value="item.value" :key="item.key">
+                            {{ item.text }}
+                        </option>
+                    </v-ons-select>
+                
+                </div>
+                
 			<br>
-	  		<div class="row">
-				<div class="col s6">
-					<button class="btn waves-effect waves-light" type="submit" v-on:click="registrarse()">Registrar</button>
-				</div>
-					<div class="col s6">
-					<button class="btn waves-effect waves-light" type="submit" name="cancelar">Cancelar</button>
-			</div>
-			</div>
+	  		
+         <div class="center">
+                <router-link to="/" ><v-ons-button  modifier="material large" class="button button--light" style="margin: 6px 0">Registrar</v-ons-button></router-link> 
+         </div>
 			
 			
    		 </form>
@@ -77,7 +105,43 @@
 </template>
 <script>
     export default {
-        name: 'regente'
+		name: 'regente',
+		data : function() {
+        return {
+           
+            titulo:{type:String},
+            titulo:'', 
+            imagen:{type:File},
+            contenido:{type:String},
+            categoria:{type:Boolean},
+            items: [
+        { text: 'Otra', value: 'Otra' },
+        { text: 'Salud', value: 'Salud' },
+        { text: 'Seguridad', value: 'Seguridad' },
+        { text: 'Arte', value: 'Arte' },
+        { text: 'Alimentación', value: 'Alimentación' }
+      ],
+      edos: [
+        { text: 'Lara', value: 'Lara' },
+        { text: 'Yaracuy', value: 'Yaracuy' },
+        { text: 'Falcón', value: 'Falcón' },
+        { text: 'Zulia', value: 'Zulia'},
+        { text: 'Aragua', value: 'Aragua' }
+      ],
+      
+      ciudad: [
+        { text: 'Barquisimeto', value: 'Barquisimeto' },
+        { text: 'San Felipe', value: 'San Felipe' },
+        { text: 'Coro', value: 'Coro' },
+        { text: 'Maracaibo', value: 'Maracaibo'},
+        { text: 'Maracay', value: 'Maracay' }
+      ],
+      selectedItem: ''
+        
+
+       }
+       
+   },
     }
 
 </script>
@@ -107,14 +171,24 @@ h5{
 	text-decoration: underline;
 	
 }
-h2{
-	color: purple;
-}
 .row1{
 	width: 100%;
     display: flex;
     align-items: center;
     flex-direction: row;
     margin-left: 0px;
+}
+
+.button--light {
+  background-color: transparent;
+  color: #9E9898;
+  border: 1px solid rgba(0,0,0,0.2); 
+}
+.button--light:active {
+  background-color: rgba(0,0,0,0.05);
+  color: #9E9898;
+  border: 1px solid rgba(0,0,0,0.2);
+  opacity: 3;
+ 
 }
 </style>
