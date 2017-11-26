901fd68b8a0d32a046ebfade7ee35e8ab498771a
joselyn <joselynserranof@gmail.com>
2017-11-26 17:21:57 -0400

    boton perfil

--- src/components/Principal.vue
+++ src/components/Principal.vue
@@ -2,7 +2,7 @@
   <v-ons-page id="app">
       <v-ons-toolbar class="toolbar--material" modifier="material" >
           <div class="center"><img src='../assets/img/rc1.png'  class="logo"></div>
-          <div class="right"><router-link to="/"><i class="material-icons" style="color: white;">power_settings_new</i></router-link></div>
+          
       </v-ons-toolbar>
     <tab></tab>
    
